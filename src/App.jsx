0f32f3ba1e59788a2d00a168a66e2ccea8f6a505
Audrius Bejeris <audriusbejeris@gmail.com>
2024-01-17 13:25:49 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import './App.css';
+import TodoList from './TodoList';
+
+function App() {
+	return (
+		<>
+			<CssBaseline />
+			<TodoList />
+		</>
+	);
+}
+
+export default App;
